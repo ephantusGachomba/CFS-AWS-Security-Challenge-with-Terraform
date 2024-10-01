4177176ef1d356c9bafb27830eb442685a8df74f
Ephantus Gachomba <ephantusgachomba@gmail.com>
2024-10-01 18:45:59 +0300

    Update appserver_sg.tf

--- modules/ec2_appserver/appserver_sg.tf
+++ modules/ec2_appserver/appserver_sg.tf
@@ -1,7 +1,7 @@
 #1. Web Server Security Group (appserver_sg):
 resource "aws_security_group" "appserver_sg" {
   name        = "appserver_sg"
-  description = "allow traffic from port 80/443 appserver elb & allow all outgoing traffic"
+  description = "allow traffic from port 443 appserver elb & allow all outgoing traffic"
   vpc_id      = var.vpc_id
 
   tags = {
@@ -9,17 +9,6 @@ resource "aws_security_group" "appserver_sg" {
   }
 }
 
-
-# Inbound rule for HTTP traffic from the app server ELB (port 80)
-resource "aws_security_group_rule" "allow_tcp_app_80" {
-  type                     = "ingress"
-  security_group_id        = aws_security_group.appserver_sg.id
-  from_port                = 80
-  to_port                  = 80
-  protocol                 = "tcp"
-  description              = "Allow HTTP from app server ELB"
-  source_security_group_id = var.appserver_elb_sg_id
-}
 
 
 # Inbound rule for HTTPS traffic from the app server ELB (port 443)
@@ -31,28 +20,6 @@ resource "aws_security_group_rule" "allow_tcp_app_443" {
   protocol                 = "tcp"
   description              = "Allow HTTPS from app server ELB"
   source_security_group_id = var.appserver_elb_sg_id
-}
-
-# Inbound rule for HTTP traffic from the Web ELB (port 80)
-resource "aws_security_group_rule" "allow_tcp_web_80" {
-  type                     = "ingress"
-  security_group_id        = aws_security_group.appserver_sg.id
-  from_port                = 80
-  to_port                  = 80
-  protocol                 = "tcp"
-  description              = "Allow HTTP from public ELB"
-  source_security_group_id = var.elb_security_group_id
-}
-
-# Inbound rule for HTTPS traffic from the Web ELB (port 443)
-resource "aws_security_group_rule" "allow_tcp_web_443" {
-  type                     = "ingress"
-  security_group_id        = aws_security_group.appserver_sg.id
-  from_port                = 443
-  to_port                  = 443
-  protocol                 = "tcp"
-  description              = "Allow HTTPS from public ELB"
-  source_security_group_id = var.elb_security_group_id
 }
 
 
